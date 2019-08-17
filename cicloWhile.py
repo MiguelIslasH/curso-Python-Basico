2c5da45515e155a58338d3d3cdc275df86238f3d
Miguel Islas Hernández <43648421+MiguelIslasH@users.noreply.github.com>
2019-08-17 12:22:52 -0500

    Create cicloWhile.py

---
+++ cicloWhile.py
@@ -0,0 +1,21 @@
+#while(condicion):
+#     hacer algo
+
+#imprimir 10 veces hola
+cadena = "Hola"
+contador = 0
+
+while contador<10:
+  print(cadena)
+  contador+=1
+
+
+#Mientras el usuario lo decida seguir ejecutando
+
+respuesta=1
+
+while(respuesta==1):
+  print("Se esta ejecutando...")
+  respuesta=int(input("Presiona 1 para continuar, otro numero para salir"))
+  
+print("Has terminado la ejecución")
